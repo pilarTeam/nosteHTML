cd82987cfad1e39f4e1206b71f4b432678a0b9ad
bashar0091 <awalbashar194@gmail.com>
2024-04-15 18:30:13 +0600

    form1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	mode: "jit",
-	content: ["./*.{html,js}"],
+	content: ["./*.{html,js}", "./form-bashar/*.{html,js}"],
 	theme: {
 		container: {
 			center: true,
